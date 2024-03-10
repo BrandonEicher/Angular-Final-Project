2cda0cf20c95a093647caea0798ee9c2d60808ba
BrandonEicher <149742991+BrandonEicher@users.noreply.github.com>
2024-03-09 19:20:35 -0500

    Added the components and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProductsComponent } from './components/products/products.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { EditProductComponent } from './components/edit-product/edit-product.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { NotfoundComponent } from './components/component/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProductsComponent,
+    AddProductComponent,
+    EditProductComponent,
+    ProductDetailsComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/product.ts
@@ -0,0 +1,8 @@
+export class Product {
+    id?: string;
+    productName: string = '';
+    flightNumbers: string = '';
+    description: string = '';
+    price: number = 0;
+    imageUrl: string = '';
+}
